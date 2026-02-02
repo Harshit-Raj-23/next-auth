d805ea8d909d4f30a657dbc8b5c5a16b639839fb
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2026-02-02 23:07:49 +0530

    Signup logic for the App

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,43 @@
+import { connect } from "@/config/dbConfig";
+import User from "@/models/userModel";
+import { sendEmail } from "@/utils/mailer";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { message: "User already exists!" },
+        { status: 400 },
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    const savedUser = await newUser.save();
+
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
+
+    return NextResponse.json({
+      message: "User created successfully!",
+      success: true,
+      savedUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
